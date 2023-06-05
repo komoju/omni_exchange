43a229c22f1f445f8b60ee350a3565e2e772a727
Metallion <akieckens@degica.com>
2023-06-05 16:35:39 +0900

    Refactored the open exchange rates provider so we can reuse some of its code

--- lib/omni_exchange/providers/open_exchange_rates.rb
+++ lib/omni_exchange/providers/open_exchange_rates.rb
@@ -6,31 +6,47 @@ module OmniExchange
   class OpenExchangeRates < Provider
     ENDPOINT_URL = 'https://openexchangerates.org/api/latest.json'
 
-    # This method returns the exchange rate, the rate at which the smallest unit of one currency (the base currency)
-    #   will be exchanged for another currency (the target currency), from Open Exchange Rate's API.
-    #   This method is called in the OmniExchange.exchange_currency method.
-    #
-    # @param base_currency: [String] the ISO Currency Code of the currency that you're exchanging from. ie. "USD", "JPY"
-    # @param target_currency: [String] the ISO Currency Code of the currency that you're exchanging to. ie. "EUR", "KRW"
-    # @ return [BigDecimal] an exchange rate is returned as a BigDecimal for precise calculation since this exchange
-    #   rate will be used to calculate an convert an exchange of currencies. However, an exception will be raised
-    #   if there is a timeout while connecting to xe.com or a timeout while reading Open Exchange Rate's API.
-    def self.get_exchange_rate(base_currency:, target_currency:)
-      config = OmniExchange.configuration.provider_config[:open_exchange_rates]
-      app_id = config[:app_id]
+    class << self
+      # This method returns the exchange rate, the rate at which the smallest unit of one currency (the base currency)
+      #   will be exchanged for another currency (the target currency), from Open Exchange Rate's API.
+      #   This method is called in the OmniExchange.exchange_currency method.
+      #
+      # @param base_currency: [String] the ISO Currency Code of the currency that you're exchanging from. ie. "USD", "JPY"
+      # @param target_currency: [String] the ISO Currency Code of the currency that you're exchanging to. ie. "EUR", "KRW"
+      # @ return [BigDecimal] an exchange rate is returned as a BigDecimal for precise calculation since this exchange
+      #   rate will be used to calculate an convert an exchange of currencies. However, an exception will be raised
+      #   if there is a timeout while connecting to xe.com or a timeout while reading Open Exchange Rate's API.
+      def get_exchange_rate(base_currency:, target_currency:)
+        body = api_get do |req|
+          req.params['base'] = base_currency
+        end
 
-      api = Faraday.new(OmniExchange::OpenExchangeRates::ENDPOINT_URL)
+        exchange_rate = body[:rates][target_currency.to_sym].to_d
+        currency_unit = get_currency_unit(base_currency).to_d
 
-      response = api.get do |req|
-        req.url "?app_id=#{app_id}&base=#{base_currency}"
-        req.options.timeout = config[:read_timeout] || OmniExchange::Configuration::DEFAULT_READ_TIMEOUT
-        req.options.open_timeout = config[:connect_timeout] || OmniExchange::Configuration::DEFAULT_CONNECTION_TIMEOUT
+        (exchange_rate * currency_unit).to_d
       end
 
-      exchange_rate = JSON.parse(response.body, symbolize_names: true)[:rates][target_currency.to_sym].to_d
-      currency_unit = get_currency_unit(base_currency).to_d
+      private
 
-      (exchange_rate * currency_unit).to_d
+      def api_get
+        api = Faraday.new(OmniExchange::OpenExchangeRates::ENDPOINT_URL)
+
+        response = api.get do |req|
+          req.params['app_id'] = config[:app_id]
+
+          req.options.timeout = config[:read_timeout] ||
+                                OmniExchange::Configuration::DEFAULT_READ_TIMEOUT
+          req.options.open_timeout = config[:connect_timeout] ||
+                                     OmniExchange::Configuration::DEFAULT_CONNECTION_TIMEOUT
+        end
+
+        JSON.parse(response.body, symbolize_names: true)
+      end
+
+      def config
+        OmniExchange.configuration.provider_config[:open_exchange_rates]
+      end
     end
 
     # when this file is required at the top of lib/omni_exchange.rb, this method call is run and allows
